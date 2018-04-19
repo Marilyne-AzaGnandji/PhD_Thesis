8a63dd3b8d03f345ff0ca98f59c026f3cf9d492b
Marilyne Aza-Gnandji <marilyne@bioinfo.cirad.fr>
2018-04-19 15:07:09 +0200

    Première tentative de résolution<==Eh ben ça marche pas encore!

--- rosalind/training.sh
+++ rosalind/training.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
+#test
 
-echo "ACGT"
+#****Transcribin' DNA in RNA
+
+#Raccourci clavier pour exécuter du shell / C-M-x i.e Ctrl+ALt+x
+
+#La commande tr pour translating<==== transcrire ADN en ARN
+
+#test
+
+for i in {1..20} ; do
+    echo "GATGGAACTTGACTACGTAAATT"
+done
+
+##display the dna sequence <==== dummy dna
+
+touch mydummydnasequence.fastq
+
+ echo "GATGGAACTTGACTACGTAAATT" < mydummydnasequence.fastq
+
+
+ #Replace all "T" by all "U"
+
+ ##Je cherche l'occurence "A" dans le fichier mydummydnasequence créé
+
+ grep "A" mydummydnasequence.fastq | tr -s "A" "U" | sort mydummydnasequence.fastq
+
+ # End of program
+
+
 
 exit 0
+
+
